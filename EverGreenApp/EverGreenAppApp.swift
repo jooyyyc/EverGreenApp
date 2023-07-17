903188ccae837c562bc082a04ce5537942b9a0ca
Joy Chang <joymchang@gmail.com>
2023-07-17 15:07:40 -0700

    Initial Commit

---
+++ EverGreenApp/EverGreenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverGreenAppApp.swift
+//  EverGreenApp
+//
+//  Created by scholar on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct EverGreenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
